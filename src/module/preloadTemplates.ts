87a46329b03789706be2818f1f0742396915e649
Matthew Swartzendruber <matthewswar@gmail.com>
2020-10-21 22:11:40 -0700

    Initial commit with a little template.json

---
+++ src/module/preloadTemplates.ts
@@ -0,0 +1,7 @@
+export const preloadTemplates = async function() {
+	const templatePaths = [
+		// Add paths to "systems/foundryvtt-mutants-and-masterminds/templates"
+	];
+
+	return loadTemplates(templatePaths);
+}
