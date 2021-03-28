6c5c629baeba1433e87cc615c58a3bac331c3e3d
Matthew Swartzendruber <matthewswar@gmail.com>
2021-03-28 00:02:33 -0700

    Added degree calculator

--- src/module/hooks.ts
+++ src/module/hooks.ts
@@ -1,6 +1,33 @@
 import Item3e from './item/entity';
 import { onActiveEffect } from './active-effects';
+import { calculateDegrees } from './dice';
 
-Hooks.on('renderChatLog', (app: any, html: JQuery<HTMLElement>, data: any) => Item3e.activateChatListeners(html));
+Hooks.on('renderChatLog', (app: Application, html: JQuery<HTMLElement>, data: any) => Item3e.activateChatListeners(html));
 Hooks.on('renderChatPopout', (app: any, html: JQuery<HTMLElement>, data: any) => Item3e.activateChatListeners(html));
 Hooks.on('applyActiveEffect', onActiveEffect);
+
+Hooks.on('renderSidebarTab', async (app: Application, html: JQuery<HTMLElement>, data: any) => {
+    if (!game.settings.get('mnm3e', 'enableDegreeCalculator')) {
+        return;
+    }
+
+    const template = 'systems/mnm3e/templates/apps/degree-calculator.html';
+    const renderedTemplate = $(await renderTemplate(template, {}));
+    const chatForm = html.find('#chat-form');
+    chatForm.after(renderedTemplate);
+
+    renderedTemplate.find('input').on('change', ev => {
+        const rollInput = renderedTemplate.find('.roll');
+        const dcInput = renderedTemplate.find('.dc');
+        const degreesOutput = renderedTemplate.find('.degrees')
+
+        const result = calculateDegrees(parseInt(dcInput.val() as string), parseInt(rollInput.val() as string));
+        if (Number.isNaN(result.degrees)) {
+            degreesOutput.text('');
+            degreesOutput.attr('class', 'degrees no-input')
+        } else {
+            degreesOutput.text(Math.abs(result.degrees));
+            degreesOutput.attr('class', `degrees ${result.cssClass}`);
+        }
+    });
+});
--- src/module/settings.ts
+++ src/module/settings.ts
@@ -1,3 +1,11 @@
 export const registerSettings = function() {
-	// Register any custom system settings here
+    // Register any custom system settings here
+    game.settings.register('mnm3e', 'enableDegreeCalculator', {
+        name: game.i18n.localize('MNM3E.OptionEnableDegreeCalculator'),
+        hint: game.i18n.localize('MNM3E.OptionEnableDegreeCalculatorHint'),
+        scope: 'client',
+        config: true,
+        type: Boolean,
+        default: true,
+    });
 }
